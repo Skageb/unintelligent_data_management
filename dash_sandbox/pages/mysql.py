52c625da7d8d29ba5c43bc8d5aa5828eea48d6ac
Skageb <skagegb@hotmail.no>
2025-03-31 13:32:30 +0200

    working on auto generation of news repport

--- dash_sandbox/pages/mysql.py
+++ dash_sandbox/pages/mysql.py
@@ -1,5 +1,5 @@
 import dash
-from dash import html, dcc, callback, Input, Output, State, dash_table, clientside_callback
+from dash import html, dcc, callback, Input, Output, State, dash_table, clientside_callback, ctx
 import dash_bootstrap_components as dbc
 import mysql.connector
 import pandas as pd
@@ -7,6 +7,7 @@ import pycountry
 import plotly.express as px
 import plotly.graph_objects as go
 import requests
+
 
 dash.register_page(__name__, path='/my_sql')
 
@@ -74,11 +75,15 @@ layout = dbc.Container([html.Div([
         html.H1("Find a Scoop"),
         html.Article('Use this tool to find a news story on an attack that is unique based on the selected category.'),
         dbc.Row(children=[
-            dbc.Input(),
-            dbc.Button('New Attack'),
-            dbc.Button('Generate Report')
+            dbc.Col(dcc.Dropdown(placeholder='Select Category', id='category-input')),
+            dbc.Col(dbc.Button('Find Attack on Category', id='new-attack-button')),
+            dbc.Col(dbc.Button('Generate Full Report', id='generate-report-button'))
         ]
         ),
+        dbc.Row(children=[
+            dbc.Col(dcc.RadioItems(id='scoop-search-option'), id='scoop-search-option-col')
+        ]),
+        html.Div(id='attack-report'),
         html.H1("Terrorism Database"),
         # Display the table
         
@@ -94,9 +99,86 @@ layout = dbc.Container([html.Div([
 ])
 
 @callback(
+    Output('category-input', 'options'),
+    Input('df', 'data')
+)
+def update_category_options(df_dict):
+    df = pd.DataFrame(df_dict)
+    return list(df.columns)
+
+@callback(
     Output('globe-graph', 'figure'),
-    Input('df', 'data'),
+    Input('df', 'data')
 )
 def rotate_globe(df_dict):
     df = pd.DataFrame(df_dict)
     return create_globe_plot(df)
+
+
+@callback(
+    Output('scoop-search-option-col', 'children'),
+    Input('category-input', 'value'),
+    State('df', 'data')
+)
+def category_input_response(category, df_dict):
+    df = pd.DataFrame(df_dict)
+    if category not in df.columns:
+        return dash.no_update
+    else:
+        if df.dtypes[category] == 'int64':
+            options = [{'label':'High Value','value':0},{'label':'Low Value','value':1}]
+        elif df.dtypes[category] == 'object':
+            options = [{'label':'Frequent Value','value':0},{'label':'Rare Value','value':1}]
+        return [html.Div('Value type from category:'), dbc.RadioItems(
+        id="scoop-search-option",
+        options=options,
+        value=0,
+        inline=True
+    )]
+
+@callback(
+    Output('attack-report', 'children'),
+    State('df', 'data'),
+    State('category-input', 'value'),
+    State('scoop-search-option', 'value'),
+    Input('generate-report-button', 'n_clicks'),
+    Input('new-attack-button', 'n_clicks')
+)
+def generate_report_button_response(df_dict, category, search_option, full_report_n_clicks, search_n_clicks):
+    df = pd.DataFrame(df_dict)
+    if ctx.triggered_id == 'generate-report-button':
+        n_clicks = full_report_n_clicks
+        report_format = 'full'
+    elif ctx.triggered_id == 'new-attack-button':
+        n_clicks = search_n_clicks
+        report_format = 'short'
+
+    else:
+        return dash.no_update
+    if n_clicks is None:
+        return dash.no_update
+    
+    #Get most frequent or least frequent value:
+    if df.dtypes[category] == 'object':
+        value_counts = df[category].value_counts().sort_index().reset_index()
+        value_counts.columns = ['value', 'count']
+        sorted_counts = value_counts.sort_values('count', ascending=search_option)
+        return [html.H5(f'Attack found from category {category}, value: {sorted_counts.iloc[0]['value']}')]
+
+    
+        
+    
+    #Get highest or lowest value
+    elif df.dtypes[category] == 'int64':
+        sorted_df = df.sort_values(category, ascending=search_option)
+        return [html.H5(f'Attack found from category {category}, value: {sorted_df.iloc[0][category]}')]
+
+
+    
+    
+    
+    
+
+
+
+    
