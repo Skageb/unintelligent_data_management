10269f4ce700b8df309e4d99ff6812da07128b8f
Per Simen Reed <persr18@uia.no>
2025-04-02 10:45:19 +0200

    replacing unknown ransom values with zero

--- data_producer.py
+++ data_producer.py
@@ -25,8 +25,8 @@ def producer_f(topic,broker_addr):
         df['nkill'] = df['nkill'].fillna(0).astype(int)
         df['natlty1'] = df['natlty1'].fillna(0).astype(int)
         df['ransom'] = df['ransom'].fillna(0).astype(int)
-        df['ransomamt'] = df['ransomamt'].fillna(0).astype(int)
-        df['ransompaid'] = df['ransompaid'].fillna(0).astype(int)
+        df['ransomamt'] = df['ransomamt'].fillna(0).clip(lower=0).astype(int)
+        df['ransompaid'] = df['ransompaid'].fillna(0).clip(lower=0).astype(int)
         df['latitude'] = df['latitude'].fillna(0).astype(float)
         df['longitude'] = df['longitude'].fillna(0).astype(float)
 
