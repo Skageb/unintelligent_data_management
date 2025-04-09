b58a5f3aa6c4b455901580d5580244a2d9663b8b
Per Simen Reed <persr18@uia.no>
2025-04-09 11:02:00 +0200

    added summary table for terror by country

--- prepare_dw.py
+++ prepare_dw.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-@author: vladimir
 Make sure you have mysql-connector installed:
     pip install mysql-connector-python
 Also, make sure you created a mysql user deuser with password depassword and granted your user all privileges    
@@ -36,11 +35,12 @@ def prepare_dw():
 
     create_table7 = "CREATE TABLE IF NOT EXISTS fatalities (fatalitiesId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, year YEAR, fatalities INT, UNIQUE KEY uniq_year_fatalities(year,fatalities))"          
     create_table8 = "CREATE TABLE IF NOT EXISTS ransom_by_country (ransomId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, year YEAR, country INT, country_txt VARCHAR(100), ransom_demanded INT, ransom_paid INT, UNIQUE KEY uniq_year_ransom(year,country))"
-    create_table9 = "CREATE TABLE IF NOT EXISTS terror_in_norway (terrorId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, year YEAR, city VARCHAR(100), fatalities INT, wounded INT, success INT, suicide INT, attacker_group VARCHAR(300), target_type VARCHAR(100), weapon_type VARCHAR(100), motive TEXT, UNIQUE KEY uniq_norway_summary (year, city, fatalities, attacker_group, target_type, weapon_type))"
+    #create_table9 = "CREATE TABLE IF NOT EXISTS terror_in_norway (terrorId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, year YEAR, city VARCHAR(100), fatalities INT, wounded INT, success INT, suicide INT, attacker_group VARCHAR(300), target_type VARCHAR(100), weapon_type VARCHAR(100), motive TEXT, UNIQUE KEY uniq_norway_summary (year, city, fatalities, attacker_group, target_type, weapon_type))"
+    create_table9 = "CREATE TABLE IF NOT EXISTS terror_by_country (terrorId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, year YEAR, country INT, country_txt VARCHAR(100), number_of_attacks INT)"
     create_table10 = "CREATE TABLE IF NOT EXISTS weapon_type (weapId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, year YEAR, weapon_type INT, weapon_type_desc VARCHAR(100), fatalities INT, wounded INT, occurences INT, UNIQUE KEY uniq_year_weapon(year,weapon_type))"
 
     try:  
-        conn = mysql.connector.connect(host='127.0.0.1', # !!! make sure you use your VM IP here !!!
+        conn = mysql.connector.connect(host='127.0.0.1',
                                   port=23306, 
                                   user='deuser',
                                   password='depassword')
@@ -65,7 +65,7 @@ def prepare_dw():
         cursor.execute("SET FOREIGN_KEY_CHECKS = 0")
         for table in ['fact_terror_event', 'dim_location', 'dim_target', 'dim_nationality',
                       'dim_weapon_type', 'dim_attack_type', 'fatalities', 'ransom_by_country',
-                      'terror_in_norway', 'weapon_type']:
+                      'terror_by_country', 'weapon_type']:
             cursor.execute(f"TRUNCATE TABLE {table}")
 
         conn.commit()
