a318a37779e2ebae59db0ab82b1d9968d0f546a4
Skageb <skagegb@hotmail.no>
2025-04-14 11:24:45 +0200

    added home page

--- dash_sandbox/pages/home.py
+++ dash_sandbox/pages/home.py
@@ -1,20 +1,55 @@
 import dash
-from dash import html, Output, Input
+from dash import html, dcc, callback, Input, Output, State, ctx
+import dash_bootstrap_components as dbc
+
 
 dash.register_page(__name__, path='/')
 
-layout = html.Div([
-    html.H1('This is our Home page'),
-    html.Div('This is our Home page content.'),
-    html.Button('Insert data', id='insert-button', n_clicks=0),
-    html.Div(id='output-div')
-])
+layout = dbc.Container(
+    dbc.Card(
+        dbc.CardBody([
+            html.H1('Welcome to the Global Terrorism Application', className='text-center mb-4'),
+            html.H4('Please select a backend to get started.', className='text-center mb-5'),
 
-@dash.callback(
-    Output('output-div', 'children'),
-    Input('insert-button', 'n_clicks'),
+            dbc.Row([
+                dbc.Col(
+                    dbc.Button(
+                        'MySQL',
+                        id='select-sql-button',
+                        href='/my_sql',
+                        color='primary',
+                        className='w-100 d-flex align-items-center justify-content-center',
+                        style={'fontSize': '36px', 'fontWeight': 'bold', 'aspectRatio': '1 / 1'}
+                    ),
+                    xs=4
+                ),
+                dbc.Col(
+                    dbc.Button(
+                        'MongoDB',
+                        id='select-mongo-button',
+                        href='/mongo_db',
+                        color='success',
+                        className='w-100 d-flex align-items-center justify-content-center',
+                        style={'fontSize': '36px', 'fontWeight': 'bold', 'aspectRatio': '1 / 1'}
+                    ),
+                    xs=4
+                ),
+                dbc.Col(
+                    dbc.Button(
+                        'Neo4j',
+                        id='select-neo-button',
+                        href='/neo4j',
+                        color='danger',
+                        className='w-100 d-flex align-items-center justify-content-center',
+                        style={'fontSize': '36px', 'fontWeight': 'bold', 'aspectRatio': '1 / 1'}
+                    ),
+                    xs=4
+                ),
+            ], className='mb-5'),
+
+            html.H5(html.I('Powered by the Unintelligent Data Management Group'), className='text-center text-muted')
+        ]),
+        className='shadow p-5 rounded bg-light'
+    )
 )
-def insert_data(n_clicks):
-    if n_clicks > 0:
-        return f'Prank btn. Number of times pranked: {n_clicks}'
-    return ""
+
